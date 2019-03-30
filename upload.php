81346bb36ba027271f742fe4f5f567c79b008e59
AnimeshN <animesh.nauti@gmail.com>
2019-03-30 19:55:07 +0530

    first commit

---
+++ upload.php
@@ -0,0 +1,3 @@
+<br />
+<b>Notice</b>:  Undefined index: fileToUpload in <b>/opt/lampp/htdocs/animesh/git/SWM-master/communitygis/upload.php</b> on line <b>3</b><br />
+File is empty.Sorry, your file was not uploaded.
